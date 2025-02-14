9664ec1f8c7ea2d072f3638ac5ce15ed9fa1cc52
Eugene Udovichenko <eugene.udovychenko@mynd.co>
2025-02-14 13:46:10 +0200

    feat: add game of life logic

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function setFieldSize() {
   numberOfColumns = fieldWidth / cellPlaceSize;
   numberOfRows = fieldHeight / cellPlaceSize;
 
-  field = Array.from(Array(numberOfRows), () => new Array(numberOfColumns));
+  field = Array.from({ length: numberOfColumns }, () => new Array(numberOfRows).fill(0));
 
   fieldCanvas = document.querySelector('#field');
   fieldCanvasContext = fieldCanvas.getContext('2d');
@@ -31,12 +31,16 @@ function setFieldSize() {
 
   generateRandomField();
   drawField();
+
+  setInterval(gameTick, 500);
 }
 
 function drawField() {
-  // draw circles
-  for (let i = 0; i < numberOfRows; i++) {
-    for (let j = 0; j < numberOfColumns; j++) {
+  fieldCanvasContext.beginPath();
+  fieldCanvasContext.clearRect(0, 0, fieldCanvas.width, fieldCanvas.height);
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfRows; j++) {
       if (field[i][j]) {
         drawCell(i, j);
       }
@@ -46,7 +50,7 @@ function drawField() {
   fieldCanvasContext.fill();
 }
 
-function drawCell(row, column) {
+function drawCell(column, row) {
   const x = (column * cellPlaceSize + cellPlaceSize / 2) * scale;
   const y = (row * cellPlaceSize + cellPlaceSize / 2) * scale;
 
@@ -55,12 +59,55 @@ function drawCell(row, column) {
   fieldCanvasContext.fillStyle = '#3498db';
 }
 
-function generateRandomField() {
-  for (let i = 0; i < numberOfRows; i++) {
-    for (let j = 0; j < numberOfColumns; j++) {
-      if (Math.random() > 0.5) {
-        field[i][j] = 1;
-      }
+function gameTick() {
+  const newField = Array.from({ length: numberOfColumns }, () => new Array(numberOfRows).fill(0));
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfRows; j++) {
+      const numberOfNeighbours = getNumberOfNeighbours(i, j);
+
+      const toBeBorn = !field[i][j] && (numberOfNeighbours === 3);
+      const stayAlive = field[i][j] && ((numberOfNeighbours === 2) || (numberOfNeighbours === 3));
+
+      newField[i][j] = (toBeBorn || stayAlive) ? 1 : 0;
     }
   }
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfRows; j++) {
+      field[i][j] = newField[i][j];
+    }
+  }
+
+  drawField();
+}
+
+function generateRandomField() {
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfRows; j++) {
+      field[i][j] = Math.random() > 0.5 ? 1 : 0;
+    }
+  }
+}
+
+function getNumberOfNeighbours(x, y) {
+  const prevX = getNormalizedCoordinate(x - 1, numberOfColumns);
+  const nextX = getNormalizedCoordinate(x + 1, numberOfColumns);
+  const prevY = getNormalizedCoordinate(y - 1, numberOfRows);
+  const nextY = getNormalizedCoordinate(y + 1, numberOfRows);
+
+  return [
+    field[prevX][prevY],
+    field[prevX][y],
+    field[prevX][nextY],
+    field[x][prevY],
+    field[x][nextY],
+    field[nextX][prevY],
+    field[nextX][y],
+    field[nextX][nextY],
+  ].reduce((a, b) => a + b);
+}
+
+function getNormalizedCoordinate(coordinate, size) {
+  return (coordinate + size) % size;
 }
